f1c34831532b307b14828733291aaa05185b593f
chesnakov2002 <dedpolikov@gmail.com>
2026-09-25 18:04:26 +0700

    Initial commit

---
+++ src/main/java/ru/academits/Countries.java
@@ -0,0 +1,40 @@
+package ru.academits;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Countries {
+    public String name;
+    public int population;
+    public List<Currency> currencies;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Currency{
+        public String code;
+        public String name;
+        public String symbol;
+
+        @Override
+        public String toString() {
+            return "{code='" + code + '\'' +
+                    ", name='" + name + '\'' +
+                    ", symbol='" + symbol + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            Currency currency = (Currency) o;
+            return Objects.equals(code, currency.code) && Objects.equals(name, currency.name) && Objects.equals(symbol, currency.symbol);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(code, name, symbol);
+        }
+    }
+}
---
+++ src/main/java/ru/academits/Main.java
@@ -0,0 +1,53 @@
+package ru.academits;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            List<Countries> countries = mapper.readValue(new File("src/main/resources/countries.json"), new TypeReference<List<Countries>>() {
+            });
+
+            int sumPopulation = 0;
+
+            for (Countries country : countries) {
+                sumPopulation += country.population;
+            }
+
+            System.out.println("Суммарная численность стран = " + sumPopulation);
+
+            List<Countries.Currency> uniqueCurrencies = new ArrayList<>();
+
+            for (Countries country : countries) {
+                if (country.currencies != null) {
+                    for (Countries.Currency currency : country.currencies) {
+                        if (!uniqueCurrencies.contains(currency)) {
+                            uniqueCurrencies.add(currency);
+                        }
+                    }
+                }
+            }
+
+            System.out.println("Перечень всех валют:" + uniqueCurrencies);
+
+            List<Countries> largeCountries = new ArrayList<>();
+
+            for (Countries country : countries) {
+                if (country.population >= 1000000) {
+                    largeCountries.add(country);
+                }
+            }
+
+            mapper.writeValue(new File("result.json"), largeCountries);
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при работе программы", e);
+        }
+    }
+}
